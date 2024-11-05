43ec3917dfff7589fb0a13f74f24a39ec299a505
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-11-05 13:01:46 +0100

    Adds CORS config

--- src/main/java/riccardogulin/u5d12/security/SecurityConfig.java
+++ src/main/java/riccardogulin/u5d12/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package riccardogulin.u5d12.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +37,7 @@ public class SecurityConfig {
 		// per OGNI richiesta che facciamo su OGNI endpoint
 		// - personalizzare il comportamento di alcune funzionalità preesistenti
 		// - aggiungere filtri personalizzati alla Filter Chain
+		httpSecurity.cors(Customizer.withDefaults()); // <-- OBBLIGATORIA SE VOGLIAMO CONFIGURARE I CORS GLOBALMENTE CON UN BEAN
 		return httpSecurity.build();
 	}
 
@@ -38,4 +45,18 @@ public class SecurityConfig {
 	PasswordEncoder getBCrypt() {
 		return new BCryptPasswordEncoder(12);
 	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.mywonderfulfrontend.com"));
+		// Mi creo una whitelist di uno o più indirizzi FRONTEND che voglio che possano accedere a questo backend.
+		// Volendo (anche se rischioso) potrei permettere l'accesso a tutti mettendo "*"
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration); // Applica ad ogni URL del mio backend la configurazione di sopra
+		return source;
+	} // Non dimentichiamoci di aggiungere httpSecurity.cors(Customizer.withDefaults()); alla filter chain!!!!
 }
