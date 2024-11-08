ac57e900757f1a1a71da6519bef9571e143544ec
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-11-08 17:32:16 +0100

    Update JWTCheckerFilter.java

--- src/main/java/riccardogulin/u5d12/security/JWTCheckerFilter.java
+++ src/main/java/riccardogulin/u5d12/security/JWTCheckerFilter.java
@@ -61,6 +61,6 @@ public class JWTCheckerFilter extends OncePerRequestFilter {
 
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-		return new AntPathMatcher().match("/**", request.getServletPath());
+		return new AntPathMatcher().match("/auth/**", request.getServletPath());
 	}
 }
